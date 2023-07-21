27f557ead517acf0d404229925c530b738e52bb9
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-07-20 21:11:54 -0500

    write test solutions

---
+++ lib/insert.sql
@@ -0,0 +1,128 @@
+
+INSERT INTO bears (
+    name, 
+    age, 
+    sex, 
+    color, 
+    temperament, 
+    alive )
+    VALUES (
+        'Mr. Chocolate',
+        23,
+        'M',
+        'brown',
+        'happy',
+        TRUE
+    );
+
+INSERT INTO bears (
+    name, 
+    age, 
+    sex, 
+    color, 
+    temperament, 
+    alive )
+    VALUES (
+        'Rowdy',
+        23,
+        'M',
+        'brown',
+        'happy',
+        TRUE
+    );
+
+INSERT INTO bears (
+    name, 
+    age, 
+    sex, 
+    color, 
+    temperament, 
+    alive )
+    VALUES (
+        'Tabitha',
+        23,
+        'M',
+        'brown',
+        'happy',
+        TRUE
+    );
+
+INSERT INTO bears (
+    name, 
+    age, 
+    sex, 
+    color, 
+    temperament, 
+    alive )
+    VALUES (
+        'Sergeant Brown',
+        23,
+        'M',
+        'brown',
+        'happy',
+        TRUE
+    );
+
+INSERT INTO bears (
+    name, 
+    age, 
+    sex, 
+    color, 
+    temperament, 
+    alive )
+    VALUES (
+        'Melissa',
+        23,
+        'M',
+        'brown',
+        'happy',
+        TRUE
+    );
+
+INSERT INTO bears (
+    name, 
+    age, 
+    sex, 
+    color, 
+    temperament, 
+    alive )
+    VALUES (
+        'Grinch',
+        23,
+        'M',
+        'brown',
+        'happy',
+        TRUE
+    );
+
+INSERT INTO bears (
+    name, 
+    age, 
+    sex, 
+    color, 
+    temperament, 
+    alive )
+    VALUES (
+        'Wendy',
+        23,
+        'M',
+        'brown',
+        'happy',
+        TRUE
+    );
+
+INSERT INTO bears (
+    name, 
+    age, 
+    sex, 
+    color, 
+    temperament, 
+    alive )
+    VALUES (
+        NULL,
+        23,
+        'M',
+        'brown',
+        'happy',
+        TRUE
+    );
